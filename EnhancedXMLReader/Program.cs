b38b602a72699c61865a55fb7e986ad0a3a79162
Luis Osuna <alosunac@outlook.com>
2019-09-26 00:12:09 -0500

    Add project files.

---
+++ EnhancedXMLReader/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnhancedXMLReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XMLHandler hanlder = new XMLHandler();
+            hanlder.ReadAllElements();
+            Console.Read();
+        }
+    }
+}
---
+++ EnhancedXMLReader/XMLHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace EnhancedXMLReader
+{
+    public class XMLHandler
+    {
+        public void ReadAllElements()
+        {
+            XElement xelement = XElement.Load("..\\..\\Yodlee.xml");
+            var data = from nm in xelement.Elements("bankAccount")
+                       //where (string)nm.Attribute("uniqueId") == "000000001"
+                       select nm;
+            Console.WriteLine("Details of Bank Accounts");
+            foreach (XElement xEle in data)
+            {
+                Console.WriteLine("FirstName: " + xEle.Element("FirstName")?.Value ?? string.Empty);
+                Console.WriteLine("Address1: " + xEle.Element("Address1")?.Value ?? string.Empty);
+                Console.WriteLine("State: " + xEle.Element("State")?.Value ?? string.Empty);
+            }
+        }
+    }
+}
